994f47ee8c7fd2c86e2297ac6c4dcffc4535ba60
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-09-11 18:55:22 +0530

    base: azure

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,21 +1,51 @@
 export default ({ env }) => [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  // {
-  //   name: ,
-  //   config: {
-  //     contentSecurityPolicy: {
-  //       useDefaults: true,
-  //       directives: {
-  //         'connect-src': ["'self'", 'https:'],
-  //         'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-  //         'media-src': ["'self'", 'data:', 'blob:'],
-  //         upgradeInsecureRequests: null,
-  //       },
-  //     },
-  //   },
-  // },
+  // 'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com", // Required for Strapi < 4.10.6; you can remove it otherwise
+            "https://market-assets.strapi.io", // Required for Strapi >= 4.10.6; you can remove it otherwise
+
+            /**
+
+             * Note: If using a STORAGE_URL, replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` with `process.env.STORAGE_URL`.
+
+             * If using a CDN URL, make sure to include that URL in the CSP headers, e.g., `process.env.STORAGE_CDN_URL`.
+
+             */
+
+            `${process.env.AZURE_SERVICE_BASE_URL}`,
+
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com", // Required for Strapi < 4.10.6; you can remove it otherwise
+            /**
+
+             * Note: If using a STORAGE_URL, replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` with `process.env.STORAGE_URL`.
+
+             * If using a CDN URL, make sure to include that URL in the CSP headers, e.g., `process.env.STORAGE_CDN_URL`.
+
+             */
+            `${process.env.AZURE_SERVICE_BASE_URL}`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   {
     name: 'strapi::cors',
     config: {
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,23 @@
-export default () => ({});
+// config/plugins.js
+
+module.exports = ({ env }) => {
+  console.log(env('AZURE_ACCOUNT_NAME'), 'CCAA', env('AZURE_SAS_TOKEN'));
+  return ({
+    upload: {
+      config: {
+        provider: 'strapi-provider-upload-azure-sa',
+        providerOptions: {
+          account: env('AZURE_ACCOUNT_NAME'),
+          sasToken: env('AZURE_SAS_TOKEN'),
+          serviceBaseURL: env('AZURE_SERVICE_BASE_URL'),
+          // accountKey: env('AZURE_ACCOUNT_KEY'),
+          containerName: env('AZURE_CONTAINER_NAME'),
+          defaultPath: env('AZURE_DEFAULT_PATH', 'uploads'),
+          // cdnBaseURL: env('AZURE_CDN_BASE_URL'),
+          defaultCacheControl: env('AZURE_DEFAULT_CACHE_CONTROL'),
+          removeCN: env('AZURE_REMOVE_CN'),
+        },
+      },
+    },
+  })
+};
