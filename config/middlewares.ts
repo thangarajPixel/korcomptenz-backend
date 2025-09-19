9bf0d21598b3cccbd1e5a8c4a7421b8f7a3f68ce
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-09-19 17:33:43 +0530

    fix: intergate local azure portal

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -13,33 +13,14 @@ export default ({ env }) => [
             "'self'",
             "data:",
             "blob:",
-            "dl.airtable.com", // Required for Strapi < 4.10.6; you can remove it otherwise
             "https://market-assets.strapi.io", // Required for Strapi >= 4.10.6; you can remove it otherwise
-
-            /**
-
-             * Note: If using a STORAGE_URL, replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` with `process.env.STORAGE_URL`.
-
-             * If using a CDN URL, make sure to include that URL in the CSP headers, e.g., `process.env.STORAGE_CDN_URL`.
-
-             */
-
-            `${process.env.AZURE_SERVICE_BASE_URL}`,
-
+            env('AZURE_SERVICE_BASE_URL'),
           ],
           "media-src": [
             "'self'",
             "data:",
             "blob:",
-            "dl.airtable.com", // Required for Strapi < 4.10.6; you can remove it otherwise
-            /**
-
-             * Note: If using a STORAGE_URL, replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` with `process.env.STORAGE_URL`.
-
-             * If using a CDN URL, make sure to include that URL in the CSP headers, e.g., `process.env.STORAGE_CDN_URL`.
-
-             */
-            `${process.env.AZURE_SERVICE_BASE_URL}`,
+            env('AZURE_SERVICE_BASE_URL'),
           ],
           upgradeInsecureRequests: null,
         },
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,21 +1,22 @@
 // config/plugins.js
 
 module.exports = ({ env }) => {
-  console.log(env('AZURE_ACCOUNT_NAME'), 'CCAA', env('AZURE_SAS_TOKEN'));
   return ({
     upload: {
       config: {
         provider: 'strapi-provider-upload-azure-sa',
         providerOptions: {
           account: env('AZURE_ACCOUNT_NAME'),
-          sasToken: env('AZURE_SAS_TOKEN'),
+          // sasToken: env('AZURE_SAS_TOKEN'),
           serviceBaseURL: env('AZURE_SERVICE_BASE_URL'),
-          // accountKey: env('AZURE_ACCOUNT_KEY'),
+          accountKey: env('AZURE_ACCOUNT_KEY'),
           containerName: env('AZURE_CONTAINER_NAME'),
           defaultPath: env('AZURE_DEFAULT_PATH', 'uploads'),
           // cdnBaseURL: env('AZURE_CDN_BASE_URL'),
-          defaultCacheControl: env('AZURE_DEFAULT_CACHE_CONTROL'),
-          removeCN: env('AZURE_REMOVE_CN'),
+          // defaultCacheControl: env('AZURE_DEFAULT_CACHE_CONTROL'),
+          // removeCN: env('AZURE_REMOVE_CN'),
+          // account: env('AZURE_ACCOUNT_NAME'),
+          // serviceBaseURL: env('AZURE_BLOB_ENDPOINT'),
         },
       },
     },
