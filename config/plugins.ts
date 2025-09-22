f07ce2f91ecd823e24013b830e85ffec35889970
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-09-22 16:54:50 +0530

    build: azure update

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,19 @@
-export default () => ({});
+// config/plugins.js
+
+module.exports = ({ env }) => {
+  return ({
+    upload: {
+      config: {
+        provider: 'strapi-provider-upload-azure-sa',
+        providerOptions: {
+          account: env('AZURE_ACCOUNT_NAME'),
+          serviceBaseURL: env('AZURE_SERVICE_BASE_URL'),
+          accountKey: env('AZURE_ACCOUNT_KEY'),
+          containerName: env('AZURE_CONTAINER_NAME'),
+          defaultPath: env('AZURE_DEFAULT_PATH'),
+          azureStorageApiVersion: '2023-11-03',
+        },
+      },
+    },
+  })
+};
