7cec1bc64629d5e426eb0a7b042255059a2f03a6
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-09-25 14:51:03 +0530

    fix: add missing field

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -37,6 +37,7 @@ export interface HomeHeader extends Struct.ComponentSchema {
     displayName: 'header';
   };
   attributes: {
+    childKey: Schema.Attribute.String & Schema.Attribute.Required;
     hasChild: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<true>;
     href: Schema.Attribute.String;
     label: Schema.Attribute.String;
