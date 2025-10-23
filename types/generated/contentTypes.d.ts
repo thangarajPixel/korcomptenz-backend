969aaf1c63d2c9551aa55d6ef2b70a19eda8d85d
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-10-23 13:10:39 +0530

    build: book demo feilds

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -388,12 +388,15 @@ export interface ApiBookDemoLeadBookDemoLead
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    email: Schema.Attribute.String;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
       'api::book-demo-lead.book-demo-lead'
     > &
       Schema.Attribute.Private;
+    name: Schema.Attribute.String;
+    organization: Schema.Attribute.String;
     publishedAt: Schema.Attribute.DateTime;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
