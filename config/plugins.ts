4d97a27caa3d625d30c02ad5e58ac5012dff1b85
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-11-07 17:01:44 +0530

    fix: slug generator

--- config/plugins.ts
+++ config/plugins.ts
@@ -15,6 +15,24 @@ module.exports = ({ env }) => {
         },
       },
     },
+    'auto-slug-manager': {
+      enabled: true,
+      config: {
+        locale: 'en',
+        enabled: true,                    // Enable/disable plugin globally
+        sourceField: 'title',             // Primary field to generate slug from
+        fallbackField: 'name',            // Fallback field if primary is empty
+        handleRichText: true,             // Process Rich Text (blocks) fields
+        addSuffixForUnique: true,         // Add suffixes for uniqueness
+        supportCyrillic: true,            // Support Cyrillic transliteration
+        updateExistingSlugs: true,        // Update existing slugs when title changes
+        slugifyOptions: {
+          lower: true,
+          strict: true,
+          locale: 'en'
+        }
+      }
+    }
     // email: {
     //   config: {
     //     provider: 'nodemailer',
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -866,9 +866,7 @@ export interface ApiCaseStudyCaseStudy extends Struct.CollectionTypeSchema {
       'oneToMany',
       'api::case-service.case-service'
     >;
-    slug: Schema.Attribute.String &
-      Schema.Attribute.Required &
-      Schema.Attribute.Unique;
+    slug: Schema.Attribute.UID<'title'>;
     technologies: Schema.Attribute.Relation<
       'oneToMany',
       'api::case-technology.case-technology'
@@ -877,6 +875,9 @@ export interface ApiCaseStudyCaseStudy extends Struct.CollectionTypeSchema {
       'case-study.testimonial-section',
       true
     >;
+    title: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.Unique;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
