db6ecb6c5ea41ded8a7c7715adc5ff573f177d38
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-11-10 18:25:00 +0530

    fix: populate image

--- src/api/career/controllers/career.ts
+++ src/api/career/controllers/career.ts
@@ -91,7 +91,8 @@ export default factories.createCoreController('api::career.career', ({ strapi })
                   populate: {
                     forms: true
                   }
-                }
+                },
+                image: true,
               }
             },
           }
