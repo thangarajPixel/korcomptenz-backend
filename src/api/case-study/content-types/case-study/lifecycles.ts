da00105d297dc17535b60ffedf024f2db0713b1c
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-11-10 14:56:03 +0530

    fix: slug space trim update

---
+++ src/api/case-study/content-types/case-study/lifecycles.ts
@@ -0,0 +1,15 @@
+
+export default {
+  beforeCreate(event) {
+    const { data } = event.params;
+    if (data.slug) {
+      data.slug = data?.slug?.trim();
+    }
+  },
+  beforeUpdate(event) {
+    const { data } = event.params;
+    if (data.slug) {
+      data.slug = data?.slug?.trim();
+    }
+  }
+};
