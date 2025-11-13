34a28b8a16640402a4934df1ac3adc3ed501c3ed
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-11-13 11:23:37 +0530

    fix: increase the set time

--- config/server.ts
+++ config/server.ts
@@ -4,4 +4,9 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  http: {
+    serverOptions: {
+      timeout: 600000, // Set timeout to 10 minutes (adjust as needed)
+    },
+  },
 });
