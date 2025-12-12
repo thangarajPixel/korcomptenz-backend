82b58c5412d6798708afb52be672b696ab383047
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-12-12 15:37:52 +0530

    fix: image populate

--- src/api/not-found/controllers/not-found.ts
+++ src/api/not-found/controllers/not-found.ts
@@ -3,5 +3,31 @@
  */
 
 import { factories } from '@strapi/strapi'
+import qs from 'qs'
+export default factories.createCoreController('api::not-found.not-found', ({ strapi }) => ({
+  async find(ctx) {
+    const populateQuery = qs.stringify({
+      populate: {
+        list: {
+          on: {
+            '"not-found.not-found"': {
+              populate: {
+                image: true,
+              },
+            },
+          }
+        }
+      },
+    }, {
+      encode: false,
+    })
 
-export default factories.createCoreController('api::not-found.not-found');
+    ctx.query = {
+      ...ctx.query,
+      ...qs.parse(populateQuery),
+    };
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
+    return { data: { ...data }, meta };
+  }
+}));
