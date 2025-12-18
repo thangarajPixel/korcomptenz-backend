d552b2e0e587644eac49650ac86276e08716f4d4
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-12-18 11:14:21 +0530

    fix: field update

--- src/api/new-room/controllers/new-room.ts
+++ src/api/new-room/controllers/new-room.ts
@@ -97,8 +97,7 @@ export default factories.createCoreController('api::new-room.new-room', ({ strap
       if (!entity) {
         return ctx.notFound('Page not found');
       }
-      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
-      return entity;
+      return this.transformResponse(entity);
     } catch (error) {
       console.log(error);
       return error;
