677b74a4819b18547b6a9ce68d23b5d2b89f2dd8
bragatheesh07 <bragatheesh@capecomsolutions.com>
2026-01-13 15:41:31 +0530

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -18,42 +18,21 @@ module.exports = ({ env }) => {
         // }
       },
     },
-    email: {
-      config: {
-        provider: 'nodemailer',
-        providerOptions: {
-          host: env('MAIL_HOST', 'smtp.example.com'),
-          port: env('MAIL_PORT', 587),
-          auth: {
-            user: env('MAIL_USERNAME', undefined),
-            pass: env('MAIL_PASSWORD', undefined),
-          },
-          // secure: false, // Port 25 → no SSL/TLS by default
-          // tls: {
-          //   rejectUnauthorized: false, // optional, helps avoid cert issues
-          // },
-          // ... any custom nodemailer options
-        },
-        settings: {
-          defaultFrom: env('MAIL_FROM', 'info@korcomptenz.com'),
-          defaultReplyTo: env('MAIL_FROM', 'info@korcomptenz.com'),
-        },
-      },
-    },
     // email: {
     //   config: {
     //     provider: 'nodemailer',
     //     providerOptions: {
-    //       host: env('MAIL_HOST', 'smtp.sendgrid.net'),
+    //       host: env('MAIL_HOST', 'smtp.example.com'),
     //       port: env('MAIL_PORT', 587),
-    //       secure: false, // false for 587 (TLS via STARTTLS)
     //       auth: {
-    //         user: 'apikey', // MUST be exactly "apikey"
-    //         pass: env('SENDGRID_API_KEY'),
+    //         user: env('MAIL_USERNAME', undefined),
+    //         pass: env('MAIL_PASSWORD', undefined),
     //       },
-    //       tls: {
-    //         rejectUnauthorized: false, // optional,️ remove in production if not needed
-    //       },
+    //       // secure: false, // Port 25 → no SSL/TLS by default
+    //       // tls: {
+    //       //   rejectUnauthorized: false, // optional, helps avoid cert issues
+    //       // },
+    //       // ... any custom nodemailer options
     //     },
     //     settings: {
     //       defaultFrom: env('MAIL_FROM', 'info@korcomptenz.com'),
@@ -61,6 +40,28 @@ module.exports = ({ env }) => {
     //     },
     //   },
     // },
+    email: {
+      config: {
+        provider: 'nodemailer',
+        providerOptions: {
+          host: env('MAIL_HOST', undefined),
+          port: env('MAIL_PORT', undefined),
+          // host: 'smtp.sendgrid.net',
+          // port: 587,
+          secure: true, // true for 465, false for 587
+          auth: {
+            user: 'apikey',
+            pass: env('SENDGRID_API_KEY', undefined)
+          },
+        },
+        settings: {
+          defaultFrom: env('MAIL_FROM', 'info@korcomptenz.com'),
+          defaultReplyTo: env('MAIL_FROM', 'info@korcomptenz.com'),
+        },
+      },
+    },
+
+
 
   })
 };
