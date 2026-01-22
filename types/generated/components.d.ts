1208d9acec2b196fa1749012092234c169bada48
bragatheesh07 <bragatheesh@capecomsolutions.com>
2026-01-22 10:20:58 +0530

    demo-banner-list update

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -799,6 +799,7 @@ export interface DemoPageDemoBannerList extends Struct.ComponentSchema {
       'oneToOne',
       'api::book-demo.book-demo'
     >;
+    isListPage: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
     list: Schema.Attribute.Component<'service.banner-section-data', true>;
   };
 }
