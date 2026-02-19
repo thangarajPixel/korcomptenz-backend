0ca5a45c30047161dd19149dac279e59693520dd
bragatheesh07 <bragatheesh@capecomsolutions.com>
2026-02-19 13:12:00 +0530

    midmarket-enterprises-list

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -3338,6 +3338,15 @@ export interface PageComponetsMidmarketEnterprises
       'page-componets.midmarket-enterprises-list',
       true
     >;
+    noOfColumn: Schema.Attribute.Enumeration<
+      [
+        'grid-cols-2',
+        'grid-cols-3',
+        'grid-cols-4',
+        'grid-cols-5',
+        'grid-cols--6',
+      ]
+    >;
     title: Schema.Attribute.String;
   };
 }
