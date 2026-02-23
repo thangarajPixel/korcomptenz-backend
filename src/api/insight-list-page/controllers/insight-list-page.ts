f55f25a41b9ef86d0dc00b1dfbda4f69c52141fa
carol-ignatius-stephen <carol.stephen@capecomsolutions.com>
2026-02-23 14:45:18 +0530

    Update insight-list-page.ts

--- src/api/insight-list-page/controllers/insight-list-page.ts
+++ src/api/insight-list-page/controllers/insight-list-page.ts
@@ -7,7 +7,9 @@ import { factories } from '@strapi/strapi'
 export default factories.createCoreController('api::insight-list-page.insight-list-page', ({ strapi }) => ({
   async find(ctx) {
     const [entity, category, service, technology] = await Promise.all([strapi.service('api::case-study-list.case-study-list').getPopulate(ctx, true),
-    strapi.service('api::insight-category.insight-category').find(ctx), strapi.service('api::case-service.case-service').find({
+    strapi.service('api::insight-category.insight-category').find({
+      populate: '*',
+    }), strapi.service('api::case-service.case-service').find({
       populate: '*',
     }), strapi.service('api::case-technology.case-technology').find({
       populate: '*',
