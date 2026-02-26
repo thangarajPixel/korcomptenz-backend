95c0603137c8a3bd48277bfeb2c8cf77d8207d13
carol-ignatius-stephen <carol.stephen@capecomsolutions.com>
2026-02-26 17:29:09 +0530

    Update newsletter-subscription.ts

--- src/api/newsletter-subscription/controllers/newsletter-subscription.ts
+++ src/api/newsletter-subscription/controllers/newsletter-subscription.ts
@@ -2,6 +2,27 @@
  * newsletter-subscription controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::newsletter-subscription.newsletter-subscription');
+export default factories.createCoreController(
+  'api::newsletter-subscription.newsletter-subscription',
+  () => ({
+    async create(ctx) {
+      try {
+        const response = await super.create(ctx);
+
+        return {
+          data: {
+            success: true,
+            ...response.data,
+            message: 'Form submitted successfully',
+          },
+        };
+
+      } catch (error) {
+        console.log(error);
+        return ctx.badRequest(error?.message || 'Something went wrong');
+      }
+    },
+  })
+);
