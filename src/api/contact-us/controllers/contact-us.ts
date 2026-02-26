150d7108b6783a89d67e8f50e911ec8e915661de
carol-ignatius-stephen <carol.stephen@capecomsolutions.com>
2026-02-26 16:28:02 +0530

    Update contact-us.ts

--- src/api/contact-us/controllers/contact-us.ts
+++ src/api/contact-us/controllers/contact-us.ts
@@ -88,17 +88,18 @@ export default factories.createCoreController('api::contact-us.contact-us', ({ s
             },
             'contact-us.news-letter': {
               populate: {
-                image: true
-              },
-              form: {
-                populate: {
-                  forms: {
-                    on: {
-                      'form-fields.newsletter-subscription': true
-                    },
+                image: true,
+                form: {
+                  populate: {
+                    forms: {
+                      on: {
+                        'form-fields.newsletter-subscription': true
+                      },
+                    }
                   }
                 }
-              }
+              },
+
             },
             'contact-us.contact-us-insight-list': {
               populate: {
