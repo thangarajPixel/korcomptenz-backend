7b69d0a17077409cc8ed571b773ceb4cdf943d38
carol-ignatius-stephen <carol.stephen@capecomsolutions.com>
2026-02-26 17:02:14 +0530

    Update newsletter-subscription.ts

--- src/api/newsletter-subscription/routes/newsletter-subscription.ts
+++ src/api/newsletter-subscription/routes/newsletter-subscription.ts
@@ -4,4 +4,19 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::newsletter-subscription.newsletter-subscription');
+export default factories.createCoreRouter('api::newsletter-subscription.newsletter-subscription',
+  {
+    config: {
+      create: {
+        policies: [
+          {
+            name: 'global::recaptcha',
+            config: {
+              action: 'newslettersubscriptions', // must match frontend action
+            },
+          },
+        ],
+      },
+    },
+  }
+);
