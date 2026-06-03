26bff1620b502a523f328d3d4dbe9c050c6cf5de
Arasu-7 <malarasu@capecomsolutions.com>
2026-06-03 11:15:56 +0530

    added recaptcha

--- src/api/sap-lead/routes/sap-lead.ts
+++ src/api/sap-lead/routes/sap-lead.ts
@@ -4,4 +4,17 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::sap-lead.sap-lead');
+export default factories.createCoreRouter('api::sap-lead.sap-lead', {
+  config: {
+    create: {
+      policies: [
+        {
+          name: 'global::recaptcha',
+          config: {
+            action: 'saplead', // must match frontend action
+          },
+        },
+      ],
+    },
+  },
+});
